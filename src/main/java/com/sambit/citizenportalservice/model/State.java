87eb676ac2f60ff048590b91d914519fdfc5d5e2
Sambit Kumar Pradhan <48365115+PSambitKumar@users.noreply.github.com>
2023-01-02 20:27:14 +0530

    State Model Created, Dynamic url Mapping Added.

---
+++ src/main/java/com/sambit/citizenportalservice/model/State.java
@@ -0,0 +1,8 @@
+
+
+package com.sambit.citizenportalservice.model;/**
+    *@Project : CitizenPortalService
+    *@Auther : Sambit Kumar Pradhan
+    *@Created On : 02/01/2023 - 8:23 PM
+*/public class State {
+}
